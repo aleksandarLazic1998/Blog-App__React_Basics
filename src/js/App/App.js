5db58af007b45e8a8c77f601f586a97b4ddd5c37
aleksandarLazic191277 <acafanto@gmail.com>
2021-06-18 15:46:01 +0200

    Made Home and styled Home Component where blog will be added

--- src/js/App/App.js
+++ src/js/App/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-// Importing Component Navbar
+// Importing Components
 import Navbar from '../components/Navbar/Navbar';
+import Home from '../components/Home/Home';
 
 function App() {
   return (
     <div className="App">
      <Navbar />
+     <Home />
     </div>
   );
 }
