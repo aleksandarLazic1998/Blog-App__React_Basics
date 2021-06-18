f1e409bff69b16008ea73b752a2b5de11c6f6d78
Aleksandar <aleksandar.lazic.p@gmail.com>
2021-06-18 23:51:40 +0200

    Added React-Router-Dom and Links for routes

--- src/js/App/App.js
+++ src/js/App/App.js
@@ -1,14 +1,28 @@
-import './App.css';
+import "./App.css";
 // Importing Components
-import Navbar from '../components/Navbar/Navbar';
-import Home from '../components/Home/Home';
+import Navbar from "../components/Navbar/Navbar";
+import CreateComponent from "../components/BlogList/BlogList";
+import Home from "../components/Home/Home";
+// Importing Router,Routes,Switch
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-     <Navbar />
-     <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          {/* For Home Route */}
+          <Route path="/">
+            <Home />
+          </Route>
+          {/* For Create New Blog Route */}
+          <Route>
+            <CreateComponent path="/create" />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/js/components/CreateBlogs/CreateBlogs.js
@@ -0,0 +1,9 @@
+const CreateBlogs = () => {
+  return (
+    <div className="create-blog-container">
+      <p className="tag">Create New Blog</p>
+    </div>
+  );
+};
+
+export default CreateBlogs;
