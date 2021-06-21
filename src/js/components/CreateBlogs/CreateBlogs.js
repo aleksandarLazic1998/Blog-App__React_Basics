13e0ff0a5cbbc781afa45a3c96a76dddb3d2910c
Aleksandar <aleksandar.lazic.p@gmail.com>
2021-06-21 11:14:59 +0200

    Added form skelleton and styles for it

--- src/js/components/CreateBlogs/CreateBlogs.js
+++ src/js/components/CreateBlogs/CreateBlogs.js
@@ -1,16 +1,20 @@
+import "./css/CreateBlogs.css";
+
 const CreateBlogs = () => {
   return (
-    <div className="create-blog-container">
-      <h2 className="tag">Create New Blog</h2>
+    <div className="create">
+      <h2>Create New Blog:</h2>
       <form>
         <label>Blog Title</label>
         <input type="text" required />
         <label>Blog Body</label>
         <textarea required></textarea>
         <label>Blog Author</label>
+        <input type="text" required />
+        <label>Gender</label>
         <select>
-          <option value="Aleksandar">ALeksandar</option>
-          <option value="Marco">Marco</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
         </select>
         <button>Add Blog</button>
       </form>
