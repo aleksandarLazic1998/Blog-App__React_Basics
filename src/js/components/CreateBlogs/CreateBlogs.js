d3e7f12b63de4fa4854d96a9c58447e1eabbaf7f
Aleksandar <aleksandar.lazic.p@gmail.com>
2021-06-21 10:53:51 +0200

    Added Form Skellet

--- src/js/components/CreateBlogs/CreateBlogs.js
+++ src/js/components/CreateBlogs/CreateBlogs.js
@@ -1,7 +1,19 @@
 const CreateBlogs = () => {
   return (
     <div className="create-blog-container">
-      <p className="tag">Create New Blog</p>
+      <h2 className="tag">Create New Blog</h2>
+      <form>
+        <label>Blog Title</label>
+        <input type="text" required />
+        <label>Blog Body</label>
+        <textarea required></textarea>
+        <label>Blog Author</label>
+        <select>
+          <option value="Aleksandar">ALeksandar</option>
+          <option value="Marco">Marco</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 };
