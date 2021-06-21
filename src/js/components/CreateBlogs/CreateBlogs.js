698002cae82cb92f6b6c90c7e06e00416b707d89
Aleksandar <aleksandar.lazic.p@gmail.com>
2021-06-21 11:36:50 +0200

    set initial values of form inputs

--- src/js/components/CreateBlogs/CreateBlogs.js
+++ src/js/components/CreateBlogs/CreateBlogs.js
@@ -1,22 +1,46 @@
 import "./css/CreateBlogs.css";
+import { useState } from "react";
 
 const CreateBlogs = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("");
+  const [gender, setGender] = useState("");
+
   return (
     <div className="create">
       <h2>Create New Blog:</h2>
       <form>
         <label>Blog Title</label>
-        <input type="text" required />
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
         <label>Blog Body</label>
-        <textarea required></textarea>
+        <textarea
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+          required
+        ></textarea>
         <label>Blog Author</label>
-        <input type="text" required />
+        <input
+          type="text"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+          required
+        />
         <label>Gender</label>
-        <select>
+        <select value={gender} onChange={(e) => setGender(e.target.value)}>
           <option value="male">Male</option>
           <option value="female">Female</option>
         </select>
         <button>Add Blog</button>
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
+        <p>{gender}</p>
       </form>
     </div>
   );
